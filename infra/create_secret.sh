33aafaedd21a3df856b53f5eb5fbd095c8802fa2
Bastian Somon <bastian.somon@gmail.com>
2023-10-23 15:15:20 +0200

    Fix: CI

--- infra/create_secret.sh
+++ infra/create_secret.sh
@@ -7,14 +7,12 @@ export $(sops -d secret.env | xargs)
 kubectl -n=laponie-gouv delete secret ${SECRET_NAME} 2> /dev/null
 kubectl -n=laponie-gouv \
 create secret generic ${SECRET_NAME} \
---from-literal="DB_USERNAME=$DB_USERNAME" \
---from-literal="DB_PASSWORD=$DB_PASSWORD" \
---from-literal="DB_URL=$DB_URL" \
---from-literal="DB_NAME=$DB_NAME" \
---from-literal="POSTGRES_PASSWORD=$DB_PASSWORD" \
---from-literal="POSTGRES_USER=$DB_USER" \
---from-literal="POSTGRES_DB=$DB_NAME" \
---from-literal="SMTP_HOST=$SMTP_HOST" \
---from-literal="SMTP_PORT=$SMTP_PORT" \
---from-literal="SMTP_USERNAME=$SMTP_USERNAME" \
---from-literal="SMTP_PASSWORD=$SMTP_PASSWORD"
+--from-literal="SPRING_DATASOURCE_URL=$SPRING_DATASOURCE_URL" \
+--from-literal="SPRING_DATASOURCE_USERNAME=$SPRING_DATASOURCE_USERNAME" \
+--from-literal="SPRING_DATASOURCE_PASSWORD=$SPRING_DATASOURCE_PASSWORD" \
+--from-literal="POSTGRES_USER=$SPRING_DATASOURCE_USERNAME" \
+--from-literal="POSTGRES_DB=laponie-gouv" \
+--from-literal="SPRING_MAIL_HOST=$SPRING_MAIL_HOST" \
+--from-literal="SPRING_MAIL_PORT=$SPRING_MAIL_PORT" \
+--from-literal="SPRING_MAIL_USERNAME=$SPRING_MAIL_USERNAME" \
+--from-literal="SPRING_MAIL_PASSWORD=$SPRING_MAIL_PASSWORD"
