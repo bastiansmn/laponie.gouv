806d06043250135ea79f292d6010766848ad712b
Bastian SOMON <bastian.somon@gmail.com>
2023-11-01 12:17:05 +0000

    Fix: smtp secrets

--- infra/create_registry_secret.sh
+++ infra/create_registry_secret.sh
@@ -1,3 +1,5 @@
+kubectl delete secret registry-creds -n laponie-gouv
+
 kubectl create secret generic registry-creds \
     --from-file=.dockerconfigjson=/root/.docker/config.json \
     --type=kubernetes.io/dockerconfigjson \
